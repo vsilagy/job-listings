ae3d2f5faf8b066ccbfba6e28046d93f33a2f94c
vsilagy <vladsilagy@gmail.com>
2022-10-19 02:43:42 -0400

    add job card in JobBoard

--- src/components/JobBoard.js
+++ src/components/JobBoard.js
@@ -1,5 +1,39 @@
-export default function JobBoard() {
+export default function JobBoard( {
+  job : {
+    company,
+    logo,
+    newJob,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools,
+  },
+}) {
+
+  let filters = []
+  filters.push(role, level)
+  filters = [...filters,...languages, ...tools];
+    
   return (
-    <h1>Hello</h1>
+    <div className="max-w-screen-xl mx-auto">
+      <div className="flex bg-white mx-4 my-8 p-6 gap-4 shadow-lg">
+        <div>
+          <img src={logo} alt={company} ></img>
+        </div>
+        <div className="flex flex-col justify-between">
+          <h4 className="text-darkCyan font-bold">{company}</h4>
+          <h3 className="text-xl font-bold">{position}</h3>
+          <p className="text-darkGrayishCyan"> {postedAt} • {contract} • {location}</p>
+        </div>
+        <div className="flex ml-auto items-center ">
+            {filters ? (filters.map((filter) => <span className="text-darkCyan font-bold text-center bg-lightGrayishCyanFilter ml-3 px-2 py-1 rounded-md">{filter}</span>)) : ""}
+        </div>
+      </div>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['League Spartan','ui-sans-serif', "sans-serif"],
+      },
+      colors: {
+        darkCyan: "hsl(180, 29%, 50%)",
+        lightGrayishCyanBg: "hsl(180, 52%, 96%)",
+        lightGrayishCyanFilter: "hsl(180, 31%, 95%)",
+        darkGrayishCyan: "hsl(180, 8%, 52%)",
+        veryDarkGrayishCyan: "hsl(180, 14%, 20%)"
+      }
+    },
   },
   plugins: [],
 }
